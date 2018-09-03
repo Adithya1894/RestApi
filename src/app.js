95cd6dda4ebf744d938c9fe749fb6c5665c74fdf
adithya1894 <adithya1894@gmail.com>
2018-09-02 21:25:11 -0400

    Added app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+const hostname = '127.0.0.1'; 
+
+const port = 3000;
+
+http.createServer((req, res)=>{
+	
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello World \n');
+
+
+}).listen(port, hostname); 
